29fd8d341afedcc1425cbf64f2fbf440faec503a
escharre17 <165943115+escharre17@users.noreply.github.com>
2024-04-05 15:15:26 -0400

    Update py1.py

--- py1.py
+++ py1.py
@@ -1,1 +1,33 @@
+ #Exercise 1
 print("this is python")
+
+# Exercise 2
+var = input("Enter your name: ",)
+print("Hello", var)
+
+#Exercise 3
+var2 = input("Enter Hours: ", )
+var3 = input("Enter Rate: ", )
+
+print("Pay: ", float(var2)*float(var3))
+
+#Exercise 4
+width = 17
+height = 12.0
+
+print(width//2)
+print(type(width//2))
+
+print(width/2.0)
+print(type(width/2.0))
+
+print(height/3)
+print(type(height/3))
+
+print(1+2*5)
+print(type(1+2*5))
+
+#Exercise 5
+ctemp = float(input("Please enter the temperature in Celcius: ", ))
+ftemp = ((9/5) * ctemp) +32
+print('The temperature in F is: ', ftemp)
