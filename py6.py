e88a482086de2d0085739079c350f85e77dd29c2
escharre17 <165943115+escharre17@users.noreply.github.com>
2024-04-08 13:37:45 -0400

    chapter 7

--- py6.py
+++ py6.py
@@ -1,14 +1,20 @@
-
-#Exercise 2
-fname = input('Enter a file name: ')
+#Exercise 1
+""" fname = input('Enter a file name: ')
 fhand = open(fname)
 count = 0
-out = -1
-numbers =[]
 for x in fhand:
-    out = x.find('X-DSPAM-Confidence:')
-    if out != -1:
-        number = float(x[(len(out)+out):(out + x.find(' '))])
-        numbers.append(number)
-        count = count + 1
-avg1 = sum(numbers)/len(numbers)
+    print(x.upper().rstrip()) """
+
+#Exercise 2
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    else:
+        pos = line.find(':') +1
+        num = line[pos:]
+        print(num)
+    print(line)
+print("Done")
+
