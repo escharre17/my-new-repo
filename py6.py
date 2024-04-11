ac2c02695d9584b9deafaf168a891d0b70936e93
escharre17 <165943115+escharre17@users.noreply.github.com>
2024-04-11 12:54:37 -0400

    chapter 8

--- py6.py
+++ py6.py
@@ -1,9 +1,9 @@
-#Exercise 1
-""" fname = input('Enter a file name: ')
+""" #Exercise 1
+fname = input('Enter a file name: ')
 fhand = open(fname)
 count = 0
 for x in fhand:
-    print(x.upper().rstrip()) """
+    print(x.upper().rstrip())
 
 #Exercise 2
 fname = input("Enter file name: ")
@@ -16,5 +16,37 @@ for line in fh:
         num = line[pos:]
         print(num)
     print(line)
-print("Done")
+print("Done") """
+
+#Exercise 3
+""" fname = input("Enter file name: ")
+fh = open(fname)
+lst1 = []
+lst = []
+for line in fh:
+    line.strip()
+    word = line.split()
+    lst1.append(word)
+for x in lst1:
+    for n in x:
+        lst.append(n)
+lst.sort()
+print(lst) """
+
+#Exercise 4
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+for line in fh:
+    words = line.split()
+    # print('Debug:', words)
+    if len(words) == 0 : continue
+    if words[0] != 'From:' : continue
+    count = count + 1
+    print(words[1])
+    
+print("There were", count, "lines in the file with From as the first word")
 
