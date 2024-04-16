4677934d857cd49ea15d19ed2eabdc55e096fd61
escharre17 <165943115+escharre17@users.noreply.github.com>
2024-04-16 11:18:12 -0400

    Update py7.py

--- py7.py
+++ py7.py
@@ -1,16 +1,30 @@
 #Exercise 2
 fname = input("Enter file name: ")
-""" if len(fname) < 1:
-    fname = "mbox-short.txt" """
+if len(fname) < 1:
+    fname = "mbox-short.txt" 
+
+fh = open(fname)
+dixt1 = {}
+for line in fh:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From': continue
+    if words[2] not in dixt1: dixt1[words[2]] = 1
+    else: dixt1[words[2]] += 1
+print(dixt1)
+
+#Exercise 3
+""" fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
 
 fh = open(fname)
 dixt1 = {}
 for line in fh:
     words = line.split()
     if len(words) == 0 : continue
-    if words[0] != 'From:' : continue
-    for c in words:
-        if c == 'From:': continue
-        dixt1[c] = dixt1.get(c, 0) + 1
-print(dixt1)
-print()
+    if c == 'From:': continue
+    #print(words)
+    for c in words: 
+        print(c)
+        dixt1[c] = dixt1.get(c, 1) + 1
+#print(dixt1) """
